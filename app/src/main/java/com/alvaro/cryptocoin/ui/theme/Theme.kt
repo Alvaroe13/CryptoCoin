d5ddd4fe99d57278e1d36f70fe14f829a15082e1
Alvaro Isea <alvaroe9448@gmail.com>
2021-12-07 12:05:56 +0100

    init commit

---
+++ app/src/main/java/com/alvaro/cryptocoin/ui/theme/Theme.kt
@@ -0,0 +1,45 @@
+package com.alvaro.cryptocoin.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = ColorPrimary,
+    primaryVariant = DarkGray,
+    secondary = TextWhite
+)
+
+private val LightColorPalette = lightColors(
+    primary = ColorPrimary,
+    primaryVariant = DarkGray,
+    secondary = TextWhite
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+//replaced 'isSystemInDarkTheme()' with 'true' in darkTheme param
+@Composable
+fun CryptoCoinTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
